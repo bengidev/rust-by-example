d0e3d932c81816835fb366b3475286b8546172c2
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-08-03 15:53:36 +0700

    Testcase: List

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,41 @@ impl Display for Point2D {
     }
 }
 
+// ===============================================================
+//
+// Testcase: List
+//
+// Implementing fmt::Display for a structure where the elements must each be handled sequentially is tricky.
+// The problem is that each write! generates a fmt::Result. Proper handling of this requires dealing with all the results.
+// Rust provides the ? operator for exactly this purpose.
+//
+
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Create a reference to the Vec<i32> stored in the List struct.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // index in `index`.
+        for (index, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if index != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}: {}", v, v + 1)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
+
 fn main() {
     // Comments
     //
@@ -141,4 +176,11 @@ fn main() {
     println!("Compare points: ");
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
+
+    // ===============================================================
+    //
+    // Testcase: List
+    //
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
